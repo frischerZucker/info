68001eb48ae718358baf1ae66f23ecfa1694f838
moritz <62518490+frischerZucker@users.noreply.github.com>
2022-10-28 08:43:02 +0200

    maxn, ordner
    programm zum finden des maximums von n zahlen,
    alles in ordner einsortiert

---
+++ maxn.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/*
+    maximum von n zahlen
+*/
+
+int main(int argc, char const *argv[])
+{
+    int arrayLength;
+
+    printf("#Zahlen: ");
+    scanf("%d", &arrayLength);
+
+    int a[arrayLength];
+
+    for (size_t i = 0; i < arrayLength; i++)
+    {
+        printf("Zahl %d: ", (int)i);
+        scanf("%d", &a[i]);
+    }
+    
+    int max = a[0];
+
+    for (size_t i = 1; i < arrayLength; i++)
+    {
+        if (a[i] > max) max = a[i];        
+    }
+    
+    printf("\nMaximum: %d\n", max);
+
+    return 0;
+}
